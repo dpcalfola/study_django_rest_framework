6825d3656ac7a4ced162cb7ff39840817a0bfaff
Fola Flor <dpcalfola@gmail.com>
2022-08-07 18:40:43 +0900

    TDD > Email normalize test code
        Test concept
            If email domain has uppercase,
            it should be changed to lowercase
    
            But local part should not be changed
    
        Definition of local part and domain
            test1@example.com
                local part: test1
                domain: example.com

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -24,3 +24,23 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users"""
+
+        # Test message
+        print('Model Test 2: Normalize email')
+
+        sample_email = [
+            # Test sample:
+            # 1. Domain - Uppercase should be changed to lowercase
+            # 2. Local part - Should not be changed
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['test2@Example.com', 'test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com']
+        ]
+
+        for email, expected in sample_email:
+            user = get_user_model().objects.create_user(email, 'samplepw123')
+            self.assertEqual(user.email, expected)
---
+++ documents/study_documents/docker_command/test_command.md
@@ -0,0 +1,6 @@
+# Test commands
+
+1. Execute test
+```shell
+docker-compose run --rm app sh -c "python manage.py test"
+```
