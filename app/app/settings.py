0b9a925e0b4a0d5d1c24c1eeea61dc4910296354
Fola Flor <dpcalfola@gmail.com>
2022-08-10 21:49:02 +0900

    CI, Fix > Module Import Error
        On settings.py
            SECRET_KEY를 가져올떄
            from 으로 .py 파일을 가져오고
            import 로 변수를 가져오는 방법으로 변경

--- app/app/settings.py
+++ app/app/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Import secret env
-from .env import secret_env
+from .env.secret_env import ENV_SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secret_env.SECRET_KEY
+SECRET_KEY = ENV_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
